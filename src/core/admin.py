92125e7796eb9e3f81fc72edf35da9b3de2e866f
Richelly Italo <richellyitalo@gmail.com>
2019-11-23 04:16:27 -0300

    modelos

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    exclude = ['user']
+    search_fields = ['name', 'phonenumber', 'email']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(user=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.register(Contact, ContactAdmin)
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    phonenumber = models.CharField(max_length=15)
+    email = models.EmailField(blank=True, null=True)
+    address = models.CharField(blank=True, null=True, max_length=255)
+
+    def __str__(self):
+        return self.name + ' - ' + self.phonenumber
