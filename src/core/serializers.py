2042fa887d2dc0027dd5994dd9076205371e3e29
Richelly Italo <richellyitalo@gmail.com>
2019-11-24 21:48:24 -0300

    Rota de login

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Contact
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password']
+
+
 class ContactSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Contact
         fields = '__all__'
--- src/phonebook/urls.py
+++ src/phonebook/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from core.viewsets import ContactViewSet
+from rest_framework.authtoken import views
 
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -25,5 +26,6 @@ router.register(r'contacts', ContactViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path(r'api/', include(router.urls))
+    path(r'api/', include(router.urls)),
+    path(r'api/login/', views.obtain_auth_token)
 ]
